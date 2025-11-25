a2dc81172ab9b84ee9281865abd86262601b9af6
Ahmed Mohamed <a7medelshhat@gmail.com>
2025-11-25 22:03:13 +0200

    Create about page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,10 +5,13 @@ function Login() {
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
@@ -37,13 +40,25 @@ function Login() {
       <div className="w-full flex justify-between text-sm mt-[-8px]">
         <p className="cursor-pointer">Forgot your password?</p>
         {currentState === "Login" ? (
-          <p className="cursor-pointer" onClick={() => setCurrentState("Sign Up")}>Create account</p>
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Sign Up")}
+          >
+            Create account
+          </p>
         ) : (
-          <p className="cursor-pointer" onClick={() => setCurrentState("Login")}>Login Here</p>
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Login")}
+          >
+            Login Here
+          </p>
         )}
       </div>
 
-      <button className="bg-black text-white font-light px-8 py-2 mt-4">{currentState === "Login" ? "Sign In" : "Sign Up"}</button>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
     </form>
   );
 }
