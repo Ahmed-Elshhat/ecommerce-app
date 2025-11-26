8c0ebd0229fb50f376bb82a5b48cf48a7fe4c1a6
Ahmed Mohamed <a7medelshhat@gmail.com>
2025-11-26 16:30:00 +0200

    Create admin app and creat base structcer and base config and create nav bar and sidebar components and .env file and login page

---
+++ admin/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./page/Add";
+import List from "./page/List";
+import Orders from "./page/Orders";
+import { useState } from "react";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+function App() {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mask-x-to-neutral-50 ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
