be231bbd34423b47b29d833e36588e4047fc0939
Ahmed Mohamed <a7medelshhat@gmail.com>
2025-11-28 23:23:36 +0200

    finish project

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -66,21 +66,41 @@ function PlaceOrder() {
 
       switch (method) {
         // API Calls for COD
-        case "cod": {
-          const response = await axios.post(
-            backendUrl + "/api/order/place",
-            orderData,
-            { headers: { token } }
-          );
+        case "cod":
+          {
+            const response = await axios.post(
+              backendUrl + "/api/order/place",
+              orderData,
+              { headers: { token } }
+            );
 
-          if (response.data.success) {
-            setCartItems({});
-            navigate("/orders");
-          } else {
-            toast.error(response.data.message);
+            if (response.data.success) {
+              setCartItems({});
+              navigate("/orders");
+            } else {
+              toast.error(response.data.message);
+            }
           }
           break;
-        }
+
+        case "stripe":
+          {
+            const responseStripe = await axios.post(
+              backendUrl + "/api/order/stripe",
+              orderData,
+              { headers: { token } }
+            );
+
+            if (responseStripe.data.success) {
+              const { session_url } = responseStripe.data;
+              window.location.replace(session_url);
+            } else {
+              toast.error(responseStripe.data.message);
+            }
+          }
+
+          break;
+
         default:
           break;
       }
